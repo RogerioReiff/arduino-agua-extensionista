c2f5020903b86a932102559d1038c2d370768436
RogerioTorio <26417976+RogerioReiff@users.noreply.github.com>
2023-11-04 17:18:41 -0300

    Atualizei código
    * Erro com DHT11 consertado
    * Consertei alguns erros
    * Adicionei novos comentários

--- projetoExtensionista.ino
+++ projetoExtensionista.ino
@@ -1,11 +1,15 @@
-#include <dht11.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define WS_POWER_PIN 7
-#define WS_SIGNAL_PIN A5
+#define WS_POWER_PIN 7 //Water level sensor
+#define WS_SIGNAL_PIN A5 //Water level sensor
+#define DHTTYPE DHT11
 #define DHT11PIN 4
-#define WT_SENSOR_PIN 13
+#define WT_SENSOR_PIN 13 //water temperature
+
+DHT dht(DHT11PIN, DHTTYPE);
 
 OneWire oneWire(WT_SENSOR_PIN); //setup do onewire
 DallasTemperature tempSensor(&oneWire); //passa o onewire para a biblioteca Dallas
@@ -33,37 +37,38 @@ void loop() {
   delay(10);
   value = analogRead(WS_SIGNAL_PIN); // ler o valor analogo do sensor
   delay(10);
-  digitalWrite(WS_POWER_PING, LOW);
+  digitalWrite(WS_POWER_PIN, LOW);
 
-  Serial.print("Valor do sensor: ");
+  Serial.print("\nComeço dos valores ");
+  Serial.print("\nValor do sensor: ");
   Serial.println(value);
 
   delay(1000);
 
   //<<---------------------DHT11--------------------->>
   Serial.println();
-  int chk = DHT11.read(DHT11PIN);
+  int chk = dht.read(DHT11PIN);
 
   Serial.print("Humidade (%): ");
-  Serial.println((float)DHT11.humidity, 2);
+  Serial.println(dht.readHumidity());
 
   Serial.print("Temperatura (C): ");
-  Serial.println((float)DHT11.temperature, 2);
+  Serial.println(dht.readTemperature());
   delay(2000);
 
   //<<---------------------WATER TEMPERATURE SENSOR--------------------->>
   tempSensor.requestTemperatures(); // pede as temperaturas
   tempCelsius = tempSensor.getTempCByIndex(0); //ler a temperatura em celsius
-  tempFahrenheit = TempCelsius * 9 /5 + 32; //converte de C para F
+  tempFahrenheit = tempCelsius * 9 /5 + 32; //converte de C para F
 
-  Serial.print("Temperatura: ");
+  Serial.print("Temperatura WTS: ");
   Serial.print(tempCelsius);
   Serial.print("°C");
   Serial.print("~");
   Serial.print(tempFahrenheit);
   Serial.print("°F");
 
-  delay(1000);
+  delay(5000);
 
 
 }
